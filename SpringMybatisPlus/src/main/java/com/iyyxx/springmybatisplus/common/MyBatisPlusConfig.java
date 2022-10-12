59e022cef4b7b75677a3a2345999d40c6102ffbd
linzh <linzhihui@hotmail.com>
2022-10-12 11:33:54 +0800

    MyBatisPlus实战,包括集成mbp、自动代码生成、模板引入及druid连接池的使用！

---
+++ SpringMybatisPlus/src/main/java/com/iyyxx/springmybatisplus/common/MyBatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.iyyxx.springmybatisplus.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @className: MyBatisPlusConfig
+ * @description: TODO 类描述
+ * @author: eric 4575252@gmail.com
+ * @date: 2022/10/12/0012 10:56:32
+ **/
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
